1accb43c895eaecf1bafff6a61f99bf8b741321c
Bivek Baral <baralbibek1234@gmail.com>
2024-08-27 08:47:22 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// a shortcut to else and if ekse statement helps to assign a variable  base on the condition? codeIftrue: codeIfFalse
+// let age = 21;
+// let message = age >= 18 ? "you are an adult" : "you are a minor";
+// console.log(message);
+// let time = 16;
+// let greeting = time <= 12 ? "good morning" : "Good afternoon";
+// console.log(greeting);
+
+// let isStudent = true;
+// let message = isStudent ? "You are a student" : "You are not a student";
+// console.log(message);
+// let purchaseAmount = 125;
+// let discount = purchaseAmount >= 100 ? 10 : 0;
+// console.log(
+//   `Your total is ${purchaseAmount - purchaseAmount * (discount / 100)}`
+// );
+let purchaseAmout = 124;
+let discount = purchaseAmout >= 100 ? 10 : 0;
+console.log(
+  `your full price after discount will be ${
+    purchaseAmout - purchaseAmout(purchaseAmout - discount / 100)
+  }`
+);
